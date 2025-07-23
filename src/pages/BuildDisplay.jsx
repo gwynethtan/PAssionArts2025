28df2478e95b4c56793d5d765b4b0901522934fe
gwynethtan <S10257317@connect.np.edu.sg>
2025-07-23 16:50:19 +0800

    refactor: changed animation code logic

--- src/pages/BuildDisplay.jsx
+++ src/pages/BuildDisplay.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-import { loadAllPersonality } from '/PAssionArts2025/firebase'; 
+import { loadAllPersonality } from '/PAssionArts2025/firebase';
 import { TaggedSingularBuild } from '../components/SingularBuild';
 import { Carousel } from "flowbite-react";
+import Slider from 'react-infinite-logo-slider'
 
 const AllSingularBuilds = () => {
   const [allBuilds, setAllBuilds] = useState([]);
@@ -26,16 +27,30 @@ const AllSingularBuilds = () => {
   }, []);
 
   return (
-    <div className="slider sm:mt-24 mt-20 flex relative w-screen h-screen overflow-x-hidden">
-      {allBuilds.map((build,index) => (
-        <div className="singularBuild" style={{ "--position": index }}>
-          <TaggedSingularBuild
-            quizChoices={build.quizChoices}
-            personalityData={build.personalityType}
-          />
+    <section className="sm:pt-18 pt-14 flex justify-center items-center min-h-screen">
+      <div>
+        <h1 className="text-main text-5xl md:text-5xl lg:text-[6vh] font-bold text-center pb-12 md:pb-20 lg:pb-6">Wait and see your build here!</h1>
+        <div className="slider flex relative w-screen overflow-x-hidden">
+          <Slider
+          width="500px"
+          duration={80}
+          pauseOnHover={false}
+          blurBorders={false}
+          blurBorderColor={'#fff'}
+          >
+            {allBuilds.map((build,index) => (
+              <Slider.Slide>
+                <TaggedSingularBuild
+                  quizChoices={build.quizChoices}
+                  personalityData={build.personalityType}
+                />
+              </Slider.Slide>
+            ))}
+          </Slider>
         </div>
-      ))}
-    </div>
+      </div>
+    </section>
+
   );
 };
 
