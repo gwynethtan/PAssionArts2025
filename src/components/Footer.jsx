cc5b70c7f0872914c44086035cdf68c5ed5358d0
gwynethtan <S10257317@connect.np.edu.sg>
2025-07-26 01:40:18 +0800

    fix: removed links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,9 @@ const Footer=()=> {
   return (
     <footer className="p-5">
         <div className="flex space-x-7 justify-center items-center lg:px-96">
-            <a href="https://github.com/gwynethtan" >
-                <img src="/passionArts.png" alt="PassionArts Icon" className="h-10 w-36" />
-            </a>
-            <a href="mailto:gwynethtann@gmail.com">
-                <img src="/sg60.png" alt="SG60 Icon" className="h-16 w-16" />
-            </a>
-            <a href="mailto:gwynethtann@gmail.com">
-                <img src="/peopleAssociation.png" alt="People Association Icon" className="h-16 w-16" />
-            </a>
+            <img src="/passionArts.png" alt="PassionArts Icon" className="h-10 w-36" />
+            <img src="/sg60.png" alt="SG60 Icon" className="h-16 w-16" />
+            <img src="/peopleAssociation.png" alt="People Association Icon" className="h-16 w-16" />
         </div>
     </footer>
   );
